2e24647141c314462496858204fd1b89067a2aa1
elbekjustin <elbekjumanazarov03@gmail.com>
2024-11-20 05:52:10 +0900

    feat: create post api request

--- src/dog/dog.controller.ts
+++ src/dog/dog.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { DogService } from './dog.service';
-import { query, Request } from 'express';
 
 @Controller('dog')
 export class DogController {
@@ -19,6 +18,13 @@ export class DogController {
         console.log('quary:', quary);
 
         return this.dogService.introduce();
+      }
+
+      @Post('edit')
+      public modifyDetail(@Body() body: any): string {
+        console.log('body:', body);
+
+        return this.dogService.modifyDetail();
       }
     
 }
